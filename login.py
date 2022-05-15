8e73643d7e3f8b4138cab7c166f39d339818f5be
Richard Kendall <richard.kendall@gmail.com>
2022-05-15 14:52:41 +1000

    adding more logging

--- login.py
+++ login.py
@@ -1,25 +1,32 @@
-import urllib, json, sys
+import urllib, json, logging
 import requests 
 import boto3 
+
+logger = logging.getLogger(__name__)
 
 sts_connection = boto3.client("sts")
 
 def generate_console_url(account_id, role):
+  logger.info(f"Getting console URL for {account_id}/{role}")
   assumed_role_object = sts_connection.assume_role(
     RoleArn=f"arn:aws:iam::{account_id}:role/{role}",
     RoleSessionName="AssumeRoleSession",
   )
+  logger.info("Assumed role")
   url_credentials = {}
   url_credentials["sessionId"] = assumed_role_object.get("Credentials").get("AccessKeyId")
   url_credentials["sessionKey"] = assumed_role_object.get("Credentials").get("SecretAccessKey")
   url_credentials["sessionToken"] = assumed_role_object.get("Credentials").get("SessionToken")
   json_string_with_temp_credentials = json.dumps(url_credentials)
 
+  logger.info("Getting signin token")
   request_parameters = "?Action=getSigninToken"
   request_parameters += "&SessionDuration=43200"
   request_parameters += "&Session=" + urllib.parse.quote_plus(json_string_with_temp_credentials)
   request_url = "https://signin.aws.amazon.com/federation" + request_parameters
   r = requests.get(request_url)
+  logger.info(f"Got response {r.text}")
+  
   # Returns a JSON document with a single element named SigninToken.
   signin_token = json.loads(r.text)
 
